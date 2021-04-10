623938b4573d488382b3d5a7068d119c5c33dd31
NazarineWasonga <nazarinewasonga48@gmail.com>
2021-04-10 16:19:54 +0300

    initial commit

--- hoodwatch/hoodwatch/settings.py
+++ hoodwatch/hoodwatch/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'neighbourhood.apps.NeighbourhoodConfig',
+    'bootstrap3',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
